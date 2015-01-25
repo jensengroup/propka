07478ca00534a352e7ac672ab957956064260e25
David Dotson <dldotson@asu.edu>
2015-01-25 15:18:44 -0700

    Molecular_container and run.single() now include a 'writeout' keyword.
    This is useful for external codes that don't need the file output, but
    would like performance gains from not waiting for a file to be written
    to disk each time.
    
    For Molecular_container.__init__(), writeout=False will suppress
    writeout of propka inputs. For run.single(), writeout=False will
    suppress writeout of both inputs and results.

--- propka/run.py
+++ propka/run.py
@@ -14,10 +14,12 @@ def main():
         my_molecule.calculate_pka()
         my_molecule.write_pka()
 
-def single(pdbfile, optargs=None):
+def single(pdbfile, optargs=None, writeout=True):
     """Run a single PROPKA calculation using *pdbfile* as input.
 
     Commandline options can be passed as a **list** in *optargs*.
+
+    *writeout* can be set to ``False`` to skip writing results to a file.
 
     .. rubric:: Example
 
@@ -27,7 +29,10 @@ def single(pdbfile, optargs=None):
     optargs = optargs if optargs is not None else []
     options, ignored_pdbfiles = propka.lib.loadOptions(*optargs, commandline=False)
 
-    my_molecule = propka.molecular_container.Molecular_container(pdbfile, options)
+    my_molecule = propka.molecular_container.Molecular_container(pdbfile, options, writeout=writeout)
     my_molecule.calculate_pka()
-    my_molecule.write_pka()
+
+    if writeout:
+        my_molecule.write_pka()
+
     return my_molecule
