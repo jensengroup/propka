726d6aa412be910e00fc368423817bb61af307dc
Nathan Baker <nathanandrewbaker@gmail.com>
2020-05-05 11:38:36 -0700

    Update __init__.py.
    Fix __all__ list and add docstring to point users to PROPKA website when
    used as module.

--- propka/__init__.py
+++ propka/__init__.py
@@ -1,5 +1,20 @@
-# propka 3.1
-# (Module renamed from original Source to propka to facilitate setuptools installation)
+"""PROPKA 3.1
 
-__all__ = ['coupled_residues', 'lib', 'parameters', 'residue', 'bonds', 'debug', 'ligand', 'pdb', 'calculator', 'determinants', 'mutate', 'protein', 'chain', 'iterative', 'output', 'protonate']
+See https://github.com/jensengroup/propka-3.1 for more information.
 
+Please cite these PROPKA references in publications:
+
+* Sondergaard, Chresten R., Mats HM Olsson, Michal Rostkowski, and Jan H. Jensen.
+"Improved Treatment of Ligands and Coupling Effects in Empirical Calculation and
+Rationalization of pKa Values." Journal of Chemical Theory and Computation 7,
+no. 7 (2011): 2284-2295.
+
+* Olsson, Mats HM, Chresten R. Sondergaard, Michal Rostkowski, and Jan H. Jensen.
+"PROPKA3: consistent treatment of internal and surface residues in empirical pKa
+predictions." Journal of Chemical Theory and Computation 7, no. 2 (2011): 525-537.
+"""
+__all__ = ["atom", "bonds", "calculations", "conformation_container",
+           "coupled_groups", "determinant", "determinants", "group",
+           "hybrid36", "iterative", "lib", "ligand_pka_values", "ligand",
+           "molecular_container", "output", "parameters", "pdb", "protonate",
+           "run", "vector_algebra", "version"]
