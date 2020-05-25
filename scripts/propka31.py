d30705d4909c36b8aa5923fb994dab4d329d62f6
Nathan Baker <nathanandrewbaker@gmail.com>
2020-05-25 14:38:20 -0700

    De-lint propka31.py.

--- scripts/propka31.py
+++ scripts/propka31.py
@@ -1,32 +1,30 @@
 #!/usr/bin/env python
+"""PROPKA script.
 
-# This is the original propka script. However, this distribute-based
-# installation moved the main() function into propka.run.main and just
-# generates a script called propka31 from the setup.py installation
-# script. You should not need to use this script.
-#
-# (Also note that there can be import problems because the script name
-# is the same as the module name; that's why the new script is called
-# propka31.)
+This is the original propka script. However, this distribute-based
+installation moved the main() function into propka.run.main and just
+generates a script called propka31 from the setup.py installation
+script. You should not need to use this script.
 
-import propka.lib, propka.molecular_container
+(Also note that there can be import problems because the script name
+is the same as the module name; that's why the new script is called
+propka31.)
+"""
+from propka.lib import loadOptions
+from propka.molecular_container import Molecular_container
+
 
 def main():
-    """
-    Reads in structure files, calculates pKa values, and prints pKa files
-    """
+    """Read in structure files, calculates pKa values, and prints pKa files."""
     # loading options, flaggs and arguments
-    options = propka.lib.loadOptions()
+    options = loadOptions([])
     pdbfiles = options.filenames
 
     for pdbfile in pdbfiles:
-        my_molecule = propka.molecular_container.Molecular_container(pdbfile, options)
+        my_molecule = Molecular_container(pdbfile, options)
         my_molecule.calculate_pka()
         my_molecule.write_pka()
 
 
 if __name__ == '__main__':
-    #import cProfile
-    #cProfile.run('main()',sort=1)
     main()
-
