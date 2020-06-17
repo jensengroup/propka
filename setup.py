1d8fa2bbbeb1cc16c8af8bfbf8e419d9a1625876
Nathan Baker <nathanandrewbaker@gmail.com>
2020-06-17 12:33:52 -0700

    Update setup.py with 3.2.0 information.

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 #! /usr/bin/python
-# PROPKA 3.1
-#
-#
-# setuptools installation of  PROPKA 3.1
+# PROPKA 3
 
 from setuptools import setup, find_packages
 
-VERSION = "3.1.1"
+VERSION = "3.2.0"
 
-setup(name="PROPKA",
-      version=VERSION,
-      description="Heuristic pKa calculations with ligands",
-      long_description="""
+setup(
+    name="PROPKA",
+    version=VERSION,
+    description="Heuristic pKa calculations with ligands", long_description="""
 PROPKA predicts the pKa values of ionizable groups in proteins (version 3.0) and
-protein-ligand complexes (version 3.1) based on the 3D structure.
+protein-ligand complexes (version 3.1 and later) based on the 3D structure.
 
 For proteins without ligands both version should produce the same result.
 
@@ -32,31 +29,25 @@ in publications:
 
 See http://propka.org/ for the PROPKA web server.
 """,
-      author="Jan H. Jensen",
-      author_email="jhjensen@chem.ku.dk",
-      license="LGPL v2.1",
-      url="http://propka.org",
-      keywords="science",
-      classifiers=[
-          'Development Status :: 6 - Mature',
-          'Environment :: Console',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
-          'Operating System :: POSIX',
-          'Programming Language :: Python',
-          'Topic :: Scientific/Engineering :: Bio-Informatics',
-          'Topic :: Scientific/Engineering :: Chemistry',
-      ],
-      packages=find_packages(exclude=['scripts']),
-      package_data = {'propka': ['*.dat', '*.cfg']},
-      #scripts = ["scripts/propka31.py"],  # use entry point below
-      entry_points = {
-        'console_scripts': [
-            'propka31 = propka.run:main',
-            ],
-        },
-      zip_safe=True,
-      python_requires='>=3.5',
-      tests_require=["pandas", "numpy"],
-      test_suite="tests",
-)
+    author="Jan H. Jensen",
+    author_email="jhjensen@chem.ku.dk",
+    license="LGPL v2.1",
+    url="http://propka.org",
+    keywords="science",
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
+        'Operating System :: POSIX', 'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        ],
+    packages=find_packages(exclude=['scripts']),
+    package_data={'propka': ['*.dat', '*.cfg']},
+    entry_points={'console_scripts': ['propka31 = propka.run:main', ]},
+    zip_safe=True,
+    python_requires='>=3.5',
+    tests_require=["pandas", "numpy"],
+    test_suite="tests",
+    )
