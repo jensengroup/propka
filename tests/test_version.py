61f39e3efc2444143f06ae29dcfb63bfd4f69206
Oliver Beckstein <orbeckst@gmail.com>
2020-07-17 18:23:04 -0700

    fix test_version
    Versioneer-generated version is 0-untagged on the branch where
    it is tested so need to add it to a valid result.

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,5 +5,6 @@ def test_version_exists():
     assert hasattr(propka, '__version__')
 
 def test_version():
-    assert re.match(r'[0-9]+\.[0-9]+', propka.__version__)
+    assert (re.match(r'[0-9]+\.[0-9]+', propka.__version__) or
+            propka.__version__.startswith('0+untagged'))
 
